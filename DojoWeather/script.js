ea0981fbd11094c75fc00786bc0e7681e8c68c1b
Brittany Francis <121970414+Bb080993@users.noreply.github.com>
2023-06-19 17:04:24 -0400

    Add files via upload

---
+++ DojoWeather/script.js
@@ -0,0 +1,13 @@
+console.log("Hello")
+
+function loadingReport() {
+    alert("Loading weather report...")
+}
+
+let popUp=document.querySelector("#popUp")
+function removePopup() {
+    popUp.remove()
+}
+
+
+//I cannot for the life of me figure out the temperature conversion. Any time I even try to have a function below, it tells me the rest of my functions are undefined. They only work again when I delete my celsius to fahrenheit function, so I am going to leave it like this for the moment. I may come back if we learn an easier way to create these functions. I do not feel like I have all the information needed at this time to complete the challenge function, even after watching the solutions video.
